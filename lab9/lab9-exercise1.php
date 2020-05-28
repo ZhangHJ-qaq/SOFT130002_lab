e382f3cc5d68caf7d021e4658797b81db0375025
ZhangHaojie-qaq <19302010021@fudan.edu.cn>
2020-05-28 14:58:21 +0800

    no message

--- lab9/lab9-exercise1.php
+++ lab9/lab9-exercise1.php
@@ -9,11 +9,29 @@
 <h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
 <?php
 //this is a php comment IN tags (will not appear)
+
+//exercise1_2
+echo "<div style='color: red'>exercise1_2</div>";
+echo "This page was generated: " . date("M dS, Y");
+echo "<br>";
+
+//exercise1_4
+echo "<div style='color: red'>exercise1_4</div>";
+echo "This page was generated: " . date("M dS, Y") . "<hr/>";
+
+//exercise1_8
+$date = date("M dS, Y");
+echo "<div style='color: red'>exercise1_8</div>";
+echo "This page was generated: " . $date . "<hr/>";
+
+//exercise1_9
+echo "<div style='color: red'>exercise1_9</div>";
 $d = date("l , F dS , Y H:i:s");
 echo "This page was generated " . $d;
 echo "</br>";
 
-
+//exercise1_11
+echo "<div style='color: red'>exercise1_11</div>";
 $isLeapYear = date("L");
 if ($isLeapYear) {
     $remainingDays = 366 - date("z");
